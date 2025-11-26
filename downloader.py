aa1f2159d314cb826c2b0b956e3dc9266f60db2d
NaikeR303 <moikotvaska2004@gmail.com>
2025-11-26 15:42:18 +0300

    Finished __init__ - Now it not only creates DB, but also fills it with given fiction

--- downloader.py
+++ downloader.py
@@ -13,7 +13,7 @@ class Downloader:
     """Give it URL from RR and it'll give you all content
     
     URL template: https://www.royalroad.com/fiction/{id}"""
-    def __init__(self, url:str):
+    def __init__(self, raw_url:str):
         # Init DB
         db_name = "chapter_db.db"
 
@@ -21,35 +21,40 @@ class Downloader:
         self.cursor = self.conn.cursor()
 
         # Create tables
-        # For foreing keys
+        # For foreign keys
         self.cursor.execute("PRAGMA foreign_keys = ON")
 
-        self.cursor.execute('''
+        self.cursor.execute(
+            '''
             CREATE TABLE IF NOT EXISTS fictions (
-                fiction_id INTEGER PRIMARY KEY,  
-                url TEXT NOT NULL,
-                cover TEXT NOT NULL,
-                title TEXT NOT NULL       
+                fiction_id  INTEGER PRIMARY KEY,  
+                url         TEXT UNIQUE,
+                cover       TEXT,
+                title       TEXT NOT NULL,
+                author      TEXT
             )
-            ''')
+            '''
+        )
         
-        self.cursor.execute('''
+        self.cursor.execute(
+            '''            
             CREATE TABLE IF NOT EXISTS chapters (
-                chapter_id INTEGER PRIMARY KEY,  
-                fiction_id INTEGER NOT NULL,  
-                url TEXT UNIQUE,              
-                date TEXT,                   
-                content TEXT,               
-                FOREIGN KEY (fiction_id) REFERENCES fictions(fiction_id)
-                    ON DELETE CASCADE       
+                chapter_id  INTEGER PRIMARY KEY,  
+                fiction_id  INTEGER NOT NULL,  
+                url         TEXT UNIQUE,              
+                date        TEXT,                   
+                content     TEXT,     
+
+                FOREIGN KEY (fiction_id) REFERENCES fictions(fiction_id) ON DELETE CASCADE       
             )
-            ''')
+            '''
+        )
 
-        self.conn.commit()
-
-        # Getting fiction ID
+        # Checking if URL is correct and getting fiction ID
         try:
-            self.fiction_id  = url.split("/")[4]
+            self.fiction_id  = raw_url.split("/")[4]
+
+            self.fiction_url = f"https://www.royalroad.com/fiction/{self.fiction_id}"
 
             if not self.fiction_id.isnumeric():
                 raise IncorrectURLError
@@ -58,6 +63,24 @@ class Downloader:
         except IndexError:
             raise IncorrectURLError
         
+        soup = BeautifulSoup(requests.get(self.fiction_url).content, "html.parser")
+
+        # Getting info
+        self.author_name = soup.find("div", class_="portlet-body").find("a").text
+        self.title = soup.find("div", class_="fic-header").find("h1").text
+
+        # Writing to DB
+        self.cursor.execute(
+            """
+            INSERT OR REPLACE INTO fictions (fiction_id, url, title, author) VALUES (?, ?, ?, ?)
+            """,
+            (self.fiction_id, self.fiction_url, self.title, self.author_name)
+        )
+
+        # Committing changes
+        self.conn.commit()
+
+
     def _get_url_list(self):
         soup = BeautifulSoup(requests.get(f"https://www.royalroad.com/fiction/{self.fiction_id}").content, "html.parser")
 
@@ -68,6 +91,6 @@ class Downloader:
 
 
 if __name__ == "__main__":
-    d = Downloader("https://www.royalroad.com/fiction/139212/the-hundred-reigns-timeloop-litrpg")
+    d = Downloader("https://www.royalroad.com/fiction/114710/engineering-magic-and-kitsune")
 
-    d._get_url_list()
+    # d._get_url_list()
